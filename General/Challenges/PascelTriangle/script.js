6e014474f61525fb7ab454bbf3bbddaec0c59587
Christian Rudder <ringthebellpepper@gmail.com>
2022-04-26 00:47:20 -0400

    pascal triangle solution

--- General/Challenges/PascelTriangle/script.js
+++ General/Challenges/PascelTriangle/script.js
@@ -1,7 +1,34 @@
 function pascelString(n){
 
+    let triList = {}
+    for(let i = 0; i < n; i++) {
+        if(i === 0) {
+            triList[1] = [1]
+        } else if(i === 1){
+            triList[2] = [1, 1]
+        }
+        else {
+            let temp = []
+            for(let j = 0; j < triList[i].length-1; j++) {
+                
+                if(j === 0) {
+                    temp.push(triList[i][j])
+                }
+
+                temp.push(triList[i][j] + triList[i][j+1])
+
+                if(j === triList[i].length-2) {
+                    temp.push(triList[i][j+1])
+                }
+            }
+        
+            triList[i+1] = temp;
+        }
+
+    }
+
+    return triList;
 }
 
-console.log(`3 
-23 
- `)
+console.log(pascelString(50))
+
